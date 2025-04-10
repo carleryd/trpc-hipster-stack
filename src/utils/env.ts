88a977454ed87717846b617833861fb4b3432bab
Christofer Ärleryd <c.arleryd@gmail.com>
2025-04-10 11:57:17 +0100

    Set env vars according to NODE_ENV

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,18 +7,20 @@ type EnvVars = {
 };
 
 export const ENV_VARS = {
-  STRAVA_CLIENT_ID: process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_PROD,
-  // process.env.NODE_ENV === "development"
-  //   ? process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_DEV
-  //   : process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_PROD,
-  STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET_PROD,
-  // process.env.NODE_ENV === "development"
-  //   ? process.env.STRAVA_CLIENT_SECRET_DEV
-  //   : process.env.STRAVA_CLIENT_SECRET_PROD,
+  // STRAVA_CLIENT_ID: process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_MAIN_ACCOUNT,
+  STRAVA_CLIENT_ID:
+    process.env.NODE_ENV === "development"
+      ? process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_TEST_ACCOUNT
+      : process.env.NEXT_PUBLIC_STRAVA_CLIENT_ID_MAIN_ACCOUNT,
+  // STRAVA_CLIENT_SECRET: process.env.STRAVA_CLIENT_SECRET_MAIN_ACCOUNT,
+  STRAVA_CLIENT_SECRET:
+    process.env.NODE_ENV === "development"
+      ? process.env.STRAVA_CLIENT_SECRET_TEST_ACCOUNT
+      : process.env.STRAVA_CLIENT_SECRET_MAIN_ACCOUNT,
   STRAVA_SESSION_SECRET: process.env.STRAVA_SESSION_SECRET,
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
   WEBSITE_URL:
     process.env.NODE_ENV === "development"
-      ? process.env.NEXT_PUBLIC_WEBSITE_URL_DEV
-      : process.env.NEXT_PUBLIC_WEBSITE_URL_PROD,
+      ? process.env.NEXT_PUBLIC_WEBSITE_URL_TEST_ACCOUNT
+      : process.env.NEXT_PUBLIC_WEBSITE_URL_MAIN_ACCOUNT,
 } satisfies EnvVars;
