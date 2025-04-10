76aeb0f6c96edde3798e3246e0ce19e52b69def7
Christofer Ärleryd <c.arleryd@gmail.com>
2025-04-10 11:20:12 +0100

    Handle session correclty on client without tapping into server only files

--- src/providers/ClientProviders/index.tsx
+++ src/providers/ClientProviders/index.tsx
@@ -1,20 +1,15 @@
 "use client";
 import { SessionProvider } from "~/providers/SessionProvider";
 import { TRPCReactProvider } from "~/trpc/client";
-import { auth } from "~/trpc/init";
 
-export default async function ClientProviders({
+export default function ClientProviders({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const session = await auth();
-
-  console.log("root session", session);
-
   return (
     <TRPCReactProvider>
-      <SessionProvider session={session}>{children}</SessionProvider>
+      <SessionProvider>{children}</SessionProvider>
     </TRPCReactProvider>
   );
 }
